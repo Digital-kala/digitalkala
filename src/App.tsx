1e699b8eb2f277a6adebace08ad93a06c27ea86a
crypticsy <animesh19basnet@gmail.com>
2024-09-28 21:06:04 +0100

    🎁 Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="justify-center align-middle content-center w-screen h-screen bg-slate-50">
+        <center>Website currently under construction . . .</center>
+      </div>
+    </>
+  );
+}
+
+export default App;
