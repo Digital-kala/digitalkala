0dbcf0ffa5f54bfc09853334fce56cb3c6b45ca4
crypticsy <animesh19basnet@gmail.com>
2024-10-16 13:59:04 +0100

    🎁 Setup for under construction home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const animationURL = "https://lottie.host/22d2de7d-c927-4728-9a88-e5ef89e76518/Z33GkkbfLT.json"
 
 function App() {
-
   return (
     <>
-      <div className="justify-center align-middle content-center w-screen h-screen bg-slate-50">
+      <div className="justify-center align-middle content-center w-screen h-screen bg-slate-50 flex flex-col items-center">
+        <div className="pb-4">
+          <DotLottieReact src={animationURL} autoplay loop  />
+        </div>
         <center>Website currently under construction . . .</center>
       </div>
     </>
