b6fbb38c7af6536f828d3aea8f82ade777d60328
crypticsy <animesh19basnet@gmail.com>
2024-12-13 21:16:21 +0000

    🧹 Updated team profile pic

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: './',  // Ensures that the base path for all resources is the root of the site
   plugins: [react()],
 });
